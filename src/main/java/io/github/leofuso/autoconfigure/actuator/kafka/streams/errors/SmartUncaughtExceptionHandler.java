d3e6f36541715e27f6f5623da8a0abee461d1727
LeoFuso <leonardofusonuzzo@gmail.com>
2022-07-22 02:11:57 -0300

    health-updates — A SmartUncaughtExceptionHandler first implementation

---
+++ src/main/java/io/github/leofuso/autoconfigure/actuator/kafka/streams/errors/SmartUncaughtExceptionHandler.java
@@ -0,0 +1,105 @@
+package io.github.leofuso.autoconfigure.actuator.kafka.streams.errors;
+
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.kafka.common.errors.NetworkException;
+import org.apache.kafka.common.errors.RetriableException;
+import org.apache.kafka.common.errors.TimeoutException;
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
+import org.springframework.core.Ordered;
+
+/**
+ * A {@link SmartUncaughtExceptionHandler} can be configured and decide either or not should recover from a thrown
+ * {@link Exception exception}.
+ */
+public class SmartUncaughtExceptionHandler implements StreamsUncaughtExceptionHandler {
+
+    /**
+     * Exceptions that may be worth to try to recover from.
+     */
+    private final Set<Class<? extends Throwable>> naiveRecoverableInclusion = Set.of(
+            NetworkException.class, // Usually a Broker reelection or something punctual */
+            TimeoutException.class, // Again, Network related.
+            RetriableException.class // The name give it away, didn't it?
+    );
+
+    /**
+     * Exceptions that we'll never try to recover from.
+     */
+    private final Set<Class<? extends Throwable>> obligatoryUnrecoverableInclusion = Set.of(
+            IllegalArgumentException.class, // Not by choice. Hard limit of the library.
+            IllegalStateException.class, // Not by choice. Hard limit of the library.
+            NullPointerException.class // Usually caused by a Poison Pill.
+    );
+
+    private final Set<UncaughtExceptionDecider> deciders;
+
+    public SmartUncaughtExceptionHandler() {
+        this(Stream.empty());
+    }
+
+    /**
+     * Creates a new instance of {@link SmartUncaughtExceptionHandler}.
+     *
+     * @param deciders used to decide either or not a
+     *                 {@link org.apache.kafka.streams.processor.internals.StreamThread thread} can be replaced.
+     */
+    public SmartUncaughtExceptionHandler(final Stream<UncaughtExceptionDecider> deciders) {
+        Objects.requireNonNull(deciders, "Stream of UncaughtExceptionDecider [deciders] is required");
+        this.deciders = deciders.sorted(Comparator.comparing(Ordered::getOrder))
+                                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    @Override
+    public StreamThreadExceptionResponse handle(Throwable exception) {
+
+        final Class<? extends Throwable> exceptionClass = exception.getClass();
+        for (Class<? extends Throwable> recoverable : naiveRecoverableInclusion) {
+            if (recoverable.isAssignableFrom(exceptionClass)) {
+                // Log
+                return StreamThreadExceptionResponse.REPLACE_THREAD;
+            }
+        }
+
+        for (Class<? extends Throwable> nonRecoverable : obligatoryUnrecoverableInclusion) {
+            if (nonRecoverable.isAssignableFrom(exceptionClass)) {
+                // Log
+                return StreamThreadExceptionResponse.SHUTDOWN_APPLICATION;
+            }
+        }
+
+        for (UncaughtExceptionDecider decider : deciders) {
+            if (decider.shouldRecover(exception)) {
+                // Also Log
+                return StreamThreadExceptionResponse.REPLACE_THREAD;
+            }
+        }
+        return StreamThreadExceptionResponse.SHUTDOWN_APPLICATION;
+    }
+
+    /**
+     * Since most exceptions are simply {@link org.apache.kafka.streams.errors.StreamsException stream exceptions}, may
+     * be wise to try to identify known errors by message patterns and handle them accordingly.
+     */
+    interface UncaughtExceptionDecider extends Ordered {
+
+        /**
+         * Inspect the exception received in a
+         * {@link org.apache.kafka.streams.processor.internals.StreamThread stream thread} and respond either or not
+         * should this {@link org.apache.kafka.streams.processor.internals.StreamThread thread} be replaced.
+         *
+         * @param exception the actual exception
+         * @return either or not to replace this
+         * {@link org.apache.kafka.streams.processor.internals.StreamThread thread}.
+         */
+        default boolean shouldRecover(Throwable exception) {
+            return false;
+        }
+
+    }
+}
